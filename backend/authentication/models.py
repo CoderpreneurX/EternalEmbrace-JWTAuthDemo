863557e2e6374043b2f81e51703293a2a5f5e5d5
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-08 09:14:52 +0530

    Created Profile Model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_user')
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} Profile'
