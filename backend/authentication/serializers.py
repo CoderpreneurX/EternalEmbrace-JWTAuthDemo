6ee8a1e124a3398b4b912ae6ccc163e56e03d95f
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-08 09:24:59 +0530

    Added User Model Serializer

---
+++ backend/authentication/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
