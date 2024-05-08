267aa95e32070eb57ae89683ce6f79bfa1aa2336
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-08 09:22:42 +0530

    Configured Corsheaders middleware & settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,10 +29,15 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    "corsheaders.middleware.CorsMiddleware",     #Corsheaders Middleware
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True   #Corsheaders setting
+
+CORS_ALLOW_CREDENTIALS = True   #Corsheaders setting
 
 ROOT_URLCONF = 'backend.urls'
 
