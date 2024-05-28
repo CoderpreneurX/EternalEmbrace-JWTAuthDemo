900e37519d9f374602527a04332d70de3c3d9a84
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-28 08:08:25 +0530

    Updated Profile Picture and Bio fields

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile_user')
-    profile_picture = models.ImageField(upload_to='profile_pictures/')
-    bio = models.TextField(max_length=250)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    bio = models.TextField(max_length=250, null=True, blank=True)
     country = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
 
