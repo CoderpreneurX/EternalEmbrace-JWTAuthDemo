02db45d9622631c8438caf6ff6d5c2095ca45d64
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-28 20:21:53 +0530

    Added Edit Profile Url

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegisterUserView, CreateUserProfileView
+from .views import RegisterUserView, CreateUserProfileView, EditUserProfileView
 
 urlpatterns = [
     path('auth/register/', RegisterUserView.as_view(), name='register-user'),
     path('auth/profile/register/', CreateUserProfileView.as_view(), name='create-user-profile'),
+    path('auth/profile/edit/', EditUserProfileView.as_view(), name='edit-user-profile'),
 ]
