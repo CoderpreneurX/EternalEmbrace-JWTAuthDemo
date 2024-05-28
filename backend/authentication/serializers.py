1e967fc0690c4ce651bf32131bc28f32932e59e2
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-28 09:45:02 +0530

    Added Profile Serializer

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+from .models import Profile
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
-
-    def validate_password(self, value):
-        validate_password(value)
-        return value
+        fields = ['username', 'email', 'first_name', 'last_name']
+    
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Profile
+        fields = '__all__'
