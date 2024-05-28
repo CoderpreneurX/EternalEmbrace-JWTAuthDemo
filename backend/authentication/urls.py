26e3b2bfd9a50fd920d4318d144d6a6f11bdf065
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-28 09:45:31 +0530

    Added Create User Profile

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterUserView
+from .views import RegisterUserView, CreateUserProfileView
 
 urlpatterns = [
     path('auth/register/', RegisterUserView.as_view(), name='register-user'),
+    path('auth/profile/register/', CreateUserProfileView.as_view(), name='create-user-profile'),
 ]
