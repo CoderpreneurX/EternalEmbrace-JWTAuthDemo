1f5a600a3ddd800bc795b845bce56913667fd426
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-28 08:24:43 +0530

    Updated User Serializer, removed unnecessary functions

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
@@ -11,7 +10,3 @@ class UserSerializer(serializers.ModelSerializer):
     def validate_password(self, value):
         validate_password(value)
         return value
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
