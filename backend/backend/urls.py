79ae252bbca4d84c1149a4ba22d32f46e847a868
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-28 08:30:43 +0530

    Added Login User and Token Refresh views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,4 +5,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authentication.urls')),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='login-user'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
 ]
