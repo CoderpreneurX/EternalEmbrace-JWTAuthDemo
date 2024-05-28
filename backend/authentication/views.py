59b4ed4c2e2dccbcd848c00b5f8df56a66668b4a
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-05-28 09:45:58 +0530

    Added Create User Profile View

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,10 +1,28 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
-from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .serializers import UserSerializer, ProfileSerializer
+from .models import Profile
 
 class RegisterUserView(generics.CreateAPIView):
     permission_classes = [AllowAny]
     authentication_classes = []
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+class CreateUserProfileView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        data = self.request.data
+
+        user.first_name = data.get('first_name')
+        user.last_name = data.get('last_name')
+        user.save()
+
+        serializer.save(user=user)
+
+        return serializer
