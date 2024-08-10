d9e426ebac365141500adfde23383b5d310d6345
Prabjeet_Jhass <prabjeet.jhass@gmail.com>
2024-08-11 02:30:04 +0530

    Removed prefix "auth/" to the urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,10 +3,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterUserView, CreateUserProfileView, EditUserProfileView, RetrieveUserProfileView
 
 urlpatterns = [
-    path('auth/user/register/', RegisterUserView.as_view(), name='register-user'),
-    path('auth/user/login/', TokenObtainPairView.as_view(), name="login-user"),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name="refresh-token"),
-    path('auth/profile/register/', CreateUserProfileView.as_view(), name='create-user-profile'),
-    path('auth/profile/edit/', EditUserProfileView.as_view(), name='edit-user-profile'),
-    path('auth/profile/', RetrieveUserProfileView.as_view(), name='retrieve-user-profile'),
+    path('user/register/', RegisterUserView.as_view(), name='register-user'),
+    path('user/login/', TokenObtainPairView.as_view(), name="login-user"),
+    path('token/refresh/', TokenRefreshView.as_view(), name="refresh-token"),
+    path('user/profile/register/', CreateUserProfileView.as_view(), name='create-user-profile'),
+    path('user/profile/edit/', EditUserProfileView.as_view(), name='edit-user-profile'),
+    path('user/profile/', RetrieveUserProfileView.as_view(), name='retrieve-user-profile'),
 ]
